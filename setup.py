66455c29de8916f782a28aad4b416b67a3a87dca
Naufal Luthfan Tasbihi <126564868+naufal012@users.noreply.github.com>
2025-06-11 10:04:44 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="naomi",
     version="0.1",
-    packages=find_packages(),
     install_requires=["torch>=1.8", "numpy", "pandas"],
+    py_modules=[           # list every .py file you want to import
+        "model",
+        "model_utils",
+        "helpers",
+        "train",
+    ],
     description="NAOMI: Non-Autoregressive Multiresolution Imputation model",
     author="Felix Liu et al.",
-    url="https://github.com/felixykliu/NAOMI",
+    url="https://github.com/naufal012/NAOMI",
 )
